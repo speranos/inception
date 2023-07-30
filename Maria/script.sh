ef7e8f4fd4e042741844f7ce48505fe84656c42b
enimv <oueldmamaamine@gmail.com>
2023-07-31 00:34:33 +0100

    maria_is_loading

--- Maria/script.sh
+++ Maria/script.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
 # Update the package list and upgrade existing packages
-sudo apt update
-sudo apt upgrade -y
+apt update
+apt upgrade -y
 
 # Install MariaDB
-sudo apt install -y mariadb-server
+apt install -y mariadb-server
 
 # Start MariaDB service
-sudo systemctl start mariadb
+service mysql start
 
-# Enable MariaDB to start on boot
-sudo systemctl enable mariadb
+
 
 # Secure the MariaDB installation
-sudo mysql_secure_installation
+# mysql_secure_installation
 
 # By running `mysql_secure_installation`, you will be prompted to set the root password,
 # remove anonymous users, disallow root login remotely, remove test databases, and
 # reload the privilege tables. Follow the prompts to secure your installation.
 
+
+mysql -u root
+
 # Check the status of MariaDB to verify that it's running
-sudo systemctl status mariadb
+
+# systemctl status mariadb
+
+
+# CREATE USER 'aoueldma'@'localhost' IDENTIFIED BY 'passtest'
