8ff482eccf75ca4c310d02ccfff7ae4cb7874494
enimv <oueldmamaamine@gmail.com>
2023-08-01 23:01:30 +0100

    maria_end

--- Maria/script.sh
+++ Maria/script.sh
@@ -10,8 +10,16 @@
 # reload the privilege tables. Follow the prompts to secure your installation.
 
 
-cat /etc/mysql/mariadb.conf.d/50-server.cnf | grep "bind"
-# mysql -u root
+# cat /etc/mysql/mariadb.conf.d/50-server.cnf | grep "bind"
+service mysql start
+# chmod 777 /var/run/mysqld/mysqld.sock
+mysql -u root -e "CREATE DATABASE my_database;"
+mysql -u root -e "CREATE USER 'my_database_user'@0.0.0.0 IDENTIFIED BY 'password123'";
+mysql -u root -e "SELECT User FROM mysql.user;"
+mysql -u root -e "SHOW DATABASES;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON my_database.* TO 'my_database_user'@0.0.0.0;"
+mysql -u root -e "SHOW GRANTS FOR 'my_database_user'@0.0.0.0;"
+ls /var/run/mysqld
 
 # Check the status of MariaDB to verify that it's running
 
