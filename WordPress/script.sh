8a2330f663e593c33f5fc59943cd2af991629370
Amine Oueldmama <aoueldma@c4r4p10.1337.ma>
2023-08-21 16:42:58 +0100

    to be continued

--- WordPress/script.sh
+++ WordPress/script.sh
@@ -6,24 +6,24 @@
 # DB_PASS="password123"
 
 # Update and install necessary packages
-apt-get update -y
-apt-get upgrade -y
-apt-get install curl -y
-apt-get install -y php7.3 php7.3-fpm php7.3-mysql default-mysql-client
+# apt-get update -y
+# apt-get upgrade -y
+# apt-get install curl -y
+# apt-get install -y php7.3 php7.3-fpm php7.3-mysql default-mysql-client
 
-# Download and set up WP-CLI
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# # Download and set up WP-CLI
+# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
+mkdir /run/php
 # Download WordPress core
 wp core download --allow-root
 
 # Create wp-config.php
-wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_CONT_NAME" --allow-root
+# wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_CONT_NAME" --allow-root
 
-# Create a directory for PHP-FPM socket
-mkdir /run/php
+# # Create a directory for PHP-FPM socket
 
-# Start PHP-FPM
+# # Start PHP-FPM
 php-fpm7.3 -F
